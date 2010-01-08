8de0ea95a5d3c3022010573ee0b96133ec3d539e
T. Suckow <T. Suckow@a1753b79-d6f9-6940-a279-87589ffbd0e6>
2010-01-08 08:41:47 +0000

    tagged release 0.1RC1 of the conjoon project

---
+++ tags/release-0.1RC1/src/corelib/js/source/groupware/email/AccountRecord.js
@@ -0,0 +1,37 @@
+/**
+ * conjoon
+ * (c) 2002-2010 siteartwork.de/conjoon.org
+ * licensing@conjoon.org
+ *
+ * $Author$
+ * $Id$
+ * $Date$
+ * $Revision$
+ * $LastChangedDate$
+ * $LastChangedBy$
+ * $URL$
+ */
+
+Ext.namespace('com.conjoon.groupware.email');
+
+com.conjoon.groupware.email.AccountRecord = Ext.data.Record.create([
+    {name : 'id',                 type : 'int'},
+    {name : 'name',               type : 'string'},
+    {name : 'address',            type : 'string'},
+    {name : 'replyAddress',       type : 'string'},
+    {name : 'isStandard',         type : 'bool'},
+    {name : 'protocol',           type : 'string'},
+    {name : 'serverInbox',        type : 'string'},
+    {name : 'serverOutbox',       type : 'string'},
+    {name : 'usernameInbox',      type : 'string'},
+    {name : 'usernameOutbox',     type : 'string'},
+    {name : 'userName',           type : 'string'},
+    {name : 'isOutboxAuth',       type : 'bool'},
+    {name : 'passwordInbox',      type : 'string'},
+    {name : 'passwordOutbox',     type : 'string'},
+    {name : 'signature',          type : 'string'},
+    {name : 'isSignatureUsed',    type : 'bool'},
+    {name : 'portInbox',          type : 'int'},
+    {name : 'portOutbox',         type : 'int'},
+    {name : 'isCopyLeftOnServer', type : 'bool'}
+]);
