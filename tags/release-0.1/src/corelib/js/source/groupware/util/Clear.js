9445c51c4dc70cb966158b80757feb13e546ff03
T. Suckow <T. Suckow@a1753b79-d6f9-6940-a279-87589ffbd0e6>
2010-03-01 12:24:23 +0000

    tagged release 0.1 of the conjoon Open Source project

---
+++ tags/release-0.1/src/corelib/js/source/groupware/util/Clear.js
@@ -0,0 +1,23 @@
+/**
+ * conjoon
+ * (c) 2002-2010 siteartwork.de/conjoon.org
+ * licensing@conjoon.org
+ *
+ * $Author$
+ * $Id$
+ * $Date$
+ * $Revision$
+ * $LastChangedDate$
+ * $LastChangedBy$
+ * $URL$
+ */
+
+Ext.namespace('com.conjoon.groupware.util');
+
+com.conjoon.groupware.util.Clear = Ext.extend(Ext.BoxComponent, {
+
+    autoEl : {
+        tag : 'div',
+        cls : 'x-form-clear'
+    }
+});
