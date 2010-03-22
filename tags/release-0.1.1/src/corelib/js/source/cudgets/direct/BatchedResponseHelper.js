527137c2f75a4e770e484e1631a784b6f2e8ff87
T. Suckow <T. Suckow@a1753b79-d6f9-6940-a279-87589ffbd0e6>
2010-03-22 09:25:57 +0000

    tagged release 0.1.1 of the conjoon Open Source Project

---
+++ tags/release-0.1.1/src/corelib/js/source/cudgets/direct/BatchedResponseHelper.js
@@ -0,0 +1,199 @@
+/**
+ * conjoon
+ * (c) 2002-2010 siteartwork.de/conjoon.org
+ * licensing@conjoon.org
+ *
+ * $Author$
+ * $Id$
+ * $Date$
+ * $Revision$
+ * $LastChangedDate$
+ * $LastChangedBy$
+ * $URL$
+ */
+
+Ext.namespace('com.conjoon.cudgets.direct');
+
+/**
+ * A helper class for easing the process of calling functions based on collected
+ * data from batched responses.
+ * See http://www.extjs.com/forum/showthread.php?t=84090.
+ *
+ *
+ * @author Thorsten Suckow-Homberg <ts@siteartwork.de>
+ *
+ * @class com.conjoon.cudgets.direct.BatchedResponseHelper
+ * @singleton
+ */
+com.conjoon.cudgets.direct.BatchedResponseHelper = function() {
+
+    var defaultTimeout = 100;
+
+    var currentConfig = {};
+
+    /**
+     * Called in the scope of this singleton.
+     */
+    var collectMessageTask = function(id)
+    {
+        if (!currentConfig[id]) {
+            throw(
+                "com.conjoon.cudgets.direct.BatchedResponseHelper."
+                +"collectMessageTask(): "+id+" was not found in the "
+                +"currentConfig property"
+            );
+        }
+
+        var cc = currentConfig[id];
+
+        cc.fn.call(cc.scope, cc.messages);
+
+        this.clearConfigForId(id);
+    };
+
+
+    /**
+     * Called in the scope of this singleton.
+     */
+    var processIfResultValidTask = function(id)
+    {
+        if (!currentConfig[id]) {
+            throw(
+                "com.conjoon.cudgets.direct.BatchedResponseHelper."
+                +"processIfResultValidTask(): "+id+" was not found in the "
+                +"currentConfig property"
+            );
+        }
+
+        var cc = currentConfig[id];
+
+        if (cc.result.indexOf(false) == -1) {
+            cc.success.call(cc.scope);
+        } else {
+            cc.failure.call(cc.scope);
+        }
+
+        this.clearConfigForId(id);
+    };
+
+
+    return {
+
+        /**
+         * Collects messages and calls fn of the passed config if no other
+         * messages are collected within a given interval.
+         *
+         * @param {Object} config
+         *  - fn The function to call once all messages have been collected.
+         *    The argument passed will be an array of collected messages
+         *  - scope The scope in which fn should be called. Defaults to
+         *    window
+         *  - timeout The timeout to wait for new messages. If no new messages
+         *    within this timeframe have been collected, fn will be called.
+         *    Defaults to defaultTimeout
+         *  - id The id used to identify the collected messages
+         *  - message an object with the following properties: text, title
+         *
+         * Note: if an object uses the BatchedResponseHelper for various tasks,
+         * each task needs a different id
+         */
+        collectMessage : function(config)
+        {
+            Ext.applyIf(config, {
+                timeout : defaultTimeout,
+                scope   : window
+            });
+
+            var id = config.id;
+            if (!currentConfig[id]) {
+                currentConfig[id] = {
+                    fn       : config.fn,
+                    scope    : config.scope,
+                    messages : [],
+                    task     : null
+                };
+            }
+
+            currentConfig[id]['messages'].push(config.message);
+
+            if (currentConfig[id]['task']) {
+                window.clearTimeout(currentConfig[id]['task']);
+            }
+
+            currentConfig[id]['task'] = collectMessageTask.defer(
+                config.timeout, this, [id]
+            );
+        },
+
+        /**
+         * Checks if config.result contains "false" or only "true" and calls either
+         * config.failure or config.success. The callbacks will be called after
+         * a given interval so outstanding objects have the ability to alter
+         * the behavior.
+         *
+         * @param {Object} config
+         *  - success The fucntion to execute when all result properties
+         *    were true
+         *  - failure The function to call if not all result properties were
+         *    set to "true"
+         *  - timeout The time in ms to wait before fn is called. Defaults to
+         *    defaultTimeout
+         *  - result The actual value of the success property
+         *  - scope The scope in which success/failure is to be called
+         *  - id The id used to identify further calls to this
+         *    method and the given config to properly call success/failure
+         *
+         * Note: if an object uses the BatchedResponseHelper for various tasks,
+         * each task needs a different id
+         */
+        processIfResultValid : function(config)
+        {
+            Ext.applyIf(config, {
+                timeout : defaultTimeout,
+                scope   : window
+            });
+
+            var id = config.id;
+            if (!currentConfig[id]) {
+                currentConfig[id] = {
+                    success : config.success,
+                    failure : config.failure,
+                    scope   : config.scope,
+                    result  : [],
+                    task    : null
+                };
+            }
+
+            currentConfig[id]['result'].push(config.result);
+
+            if (currentConfig[id]['task']) {
+                window.clearTimeout(currentConfig[id]['task']);
+            }
+
+            currentConfig[id]['task'] = processIfResultValidTask.defer(
+                config.timeout, this, [id]
+            );
+        },
+
+        /**
+         * Clears any timeout associated with the given id and removes the
+         * configuration for this id entirely.
+         *
+         * @param {String} id The id for which previously a config was set up
+         */
+        clearConfigForId : function(id)
+        {
+            if (currentConfig[id]) {
+                if (currentConfig[id]['task']) {
+                    window.clearTimeout(currentConfig[id]['task']);
+                }
+                currentConfig[id] = null;
+                delete currentConfig[id];
+            }
+        }
+
+
+
+    };
+
+}();
